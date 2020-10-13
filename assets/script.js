0a35304b2cb3cd496f159408670f19dcd5edf9d6
sbiego <forsesim@anche.no>
2020-10-13 04:05:58 +0200

    some min fixes; js works; now is simple to create a new page from template

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+let tracklist = document.querySelector('#tracklist')
+
+// sets the custom (if set) numbering to track list order
+if (typeof first_track !== 'undefined') {
+    tracklist.setAttribute('start', first_track);
+}
+
+// array contains everything needed
+album.forEach(function (track) {
+    let li = document.createElement('li');
+    li.innerHTML += track.title;
+    li.classList.add('track');
+    li.setAttribute('data-audiosrc', track.path);
+    tracklist.appendChild(li);
+});
+
+// Pure JS simple click (entry) and play (audiosrc) player UI
+function playTrack(entry) {
+    document.querySelectorAll('.track').forEach(element => {
+        element.classList.remove('active');
+    }); entry.classList.add('active');
+
+    var player = document.querySelector('#player>audio');
+    var playing = document.querySelector('#player>#now_playing');
+
+    player.setAttribute('src', entry.getAttribute('data-audiosrc'));
+    player.play();
+    playing.innerHTML = entry.innerHTML;
+}
+
+// binds click on tracks title to the function above
+document.querySelectorAll('.track').forEach(element => {
+    element.addEventListener('click', function() {
+        playTrack(element);
+    });
+});
